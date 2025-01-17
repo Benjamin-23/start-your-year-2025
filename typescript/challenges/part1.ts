a8f45fd745d8791c32071bd90d1daa00f982921b
benjamin-23 <kitongabenja34@gmail.com>
2025-01-17 11:12:05 +0300

    javascript and typescript with deno

---
+++ typescript/challenges/part1.ts
@@ -0,0 +1,10 @@
+function plus(x: number | string | undefined, y: number | string | undefined) {
+  if (typeof x === "number" && typeof y === "number") return x + y;
+  if (typeof x === "string" && typeof y === "string") return x + y;
+  // if (typeof x === "undefined" && typeof y === "undefined") return undefined;
+  return undefined;
+}
+
+console.log(plus(2025, 1));
+console.log(plus(undefined, 21));
+console.log(plus("StackUp", "Learn"));
